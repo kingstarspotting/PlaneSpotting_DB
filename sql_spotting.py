6334574dc99d9c2ea151ec3d3d2a54a410c646f2
kingstarspotting <nagr3@orange.fr>
2024-04-14 22:59:47 +0200

    Correction bug d'ouverture de la DB

--- sql_spotting.py
+++ sql_spotting.py
@@ -6,7 +6,7 @@ path = get_database()
 spotting_db = ("""Id_spott, Immatriculation, Modele, Msn, Compagnie, Militaire, Service, Livree, Date, Aeroport, Catalogue, Commentaire, Achievement, Image""")
 
 
-def ajout_spott(imm, mod, msn, comp, mil, ser, liv, dat, aer, cat, comm, ach, img):
+def ajout_spott(imm: str, mod: str, msn: int, comp: str, mil: int, ser: int, liv: str, dat: str, aer: str, cat: str, comm: str, ach: str, img: str):
     # Champs
     champs = spotting_db[len("Id_spott, "):]
     # Initialisation de la db
@@ -14,3 +14,5 @@ def ajout_spott(imm, mod, msn, comp, mil, ser, liv, dat, aer, cat, comm, ach, im
     cursor = con.cursor()
     print(f"""INSERT INTO Spotting({champs}) VALUES(?, ?)""")
 
+print(path)
+ajout_spott("F-ABCD", "A320-200", 217, "Air France", 0, 1, "", "2023/04/11", "LFBO", "Spotting à LFBO", "Test", "A remplir auto", "C:Images")
