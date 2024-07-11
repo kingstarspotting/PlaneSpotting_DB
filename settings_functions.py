4d27939d7a2a293eff8180a675795f1414aa7fa4
kingstarspotting <nagr3@orange.fr>
2024-07-11 15:46:53 +0200

    Update Langues 1

--- settings_functions.py
+++ settings_functions.py
@@ -16,32 +16,6 @@ def get_database():
     config.read("Settings\\config.ini")
     return config["path"]["main_db"]
 
-def get_language():
-    """
-    Permet d'obtenir la langue définie dans les paramètres
-
-    OUT:
-        - La langue à utiliser
-    """
-    config = ConfigParser()
-    config.read("Settings\\config.ini")
-    return config["settings"]["language"]
-
-def get_text_lang(section: str, var: str) -> str:
-    """
-    Fonction qui permet d'obtenir un texte en fonction de la langue sélectionnée par l'utilisateur
-    \n
-    IN:\n
-        - section (str): nom de la section où se trouve le texte (exemple: menu)
-        - var (str): variable du texte à afficher\n
-    OUT:\n
-        - Le texte à afficher
-    """
-    lang = get_language().lower() # Récupération de la langue
-    config = ConfigParser()
-    config.read(f"Languages\\{lang}.ini") # Ouverture du fichier de la langue
-    return config[section][var] # Renvoie le texte à afficher
-
 def get_color_theme():
     """
     Permet d'obtenir le thème de couleur sélectionnée par l'utilisateur et la couleur secondaire
